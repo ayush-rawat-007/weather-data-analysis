75734d437f1dc44b8adae1a356daf3d46ebd5d48
Ayush Rawat <rawatayush17427@gmail.com>
2026-05-27 16:00:25 +0530

    Initial commit - weather project

---
+++ Weather-Analytics-Project-main/main.py
@@ -0,0 +1,59 @@
+import time
+import requests
+import mysql.connector
+from datetime import datetime
+
+api_key = "d2f4324dd18fc52809a360dd11155eba"
+city = "Noida"
+
+while True:
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+        response = requests.get(url)
+        data = response.json()
+
+        city_name = data['name']
+        temp = data['main']['temp']
+        humidity = data['main']['humidity']
+        weather = data['weather'][0]['main']
+        
+        # Connect to MySQL and insert data
+        time_now = datetime.now()
+
+        conn = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="9868038712",
+            database="weather_db"
+        )
+
+        cursor = conn.cursor() # Create a cursor object to execute SQL queries
+
+        # Check for duplicate entry based on timestamp
+        check_query = "SELECT COUNT(*) FROM weather_data WHERE timestamp = %s"
+        cursor.execute(check_query, (time_now,))
+        result = cursor.fetchone()[0]
+
+        if result == 0:
+            query = """
+            INSERT INTO weather_data (city, temperature, humidity, weather, timestamp)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+
+            values = (city_name, temp, humidity, weather, time_now)
+
+            cursor.execute(query, values)
+            conn.commit()
+
+            print("Inserted ✅")
+        else:
+            print("Duplicate skipped ❌")
+
+        cursor.close()
+        conn.close()
+
+    except Exception as e:
+        print("Error:", e)
+
+    time.sleep(60)
---
+++ Weather-Analytics-Project-main/queries.sql
@@ -0,0 +1,10 @@
+
+
+USE weather_db;
+CREATE TABLE weather_data (
+    city VARCHAR(50),
+    temperature FLOAT,
+    humidity INT,
+    weather VARCHAR(50),
+    timestamp DATETIME
+);
